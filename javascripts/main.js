520ffa49ccc3be5b693fc6f4cc2a35cc0ef4a9f3
esther-ng <esther.hc.ng@gmail.com>
2016-01-25 21:52:40 -0500

    Add Javascript

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,19 @@
-console.log('This would be the main JS file.');
+// these are the two buttons
+var showButton = document.getElementById("modal-show");
+var hideButton = document.getElementById("modal-hide");
+// this is the modal element
+var modal = document.getElementById("modal-container");
+// this function shows the modal
+var showModal = function() {
+    modal.style.display = 'block';
+}
+ 
+// this function hides the modal
+var hideModal = function() {
+    modal.style.display = 'none';
+}
+// when the buttons are clicked, call functions by name
+showButton.onclick = showModal;
+hideButton.onclick = hideModal;
+ 
+
